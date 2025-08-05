bb9eea1ae61d0bdc3a4bf7228772c381edba26be
NguyenHoang5885 <NguyenHoang5885>
2025-08-05 15:06:59 +0700

    revise PID

--- src/control_engine.cpp
+++ src/control_engine.cpp
@@ -2,9 +2,8 @@
 #include "control_engine.h"
 
 float Kp = 10, Ki = 0.0, Kd = 50;
-float preError = 0, currentAngle   = 0, error = 0 ;
+float preError = 0, error = 0 ;
 float PID_integral  = 0, PID_derivative = 0;
-int   PID_output    = 0;
 extern float startPoint;
 
 void Control(int pwm){
@@ -12,8 +11,8 @@ void Control(int pwm){
     //Serial.println(pwm);
     if(pwm < 30 && pwm >= -30 ){
         pwm = 0;
-        analogWrite(EN1, -pwm) ; digitalWrite(IN1, LOW); digitalWrite(IN2, LOW); 
-        analogWrite(EN2, -pwm) ; digitalWrite(IN3, LOW ); digitalWrite(IN4, LOW); 
+        analogWrite(EN1, pwm) ; digitalWrite(IN1, LOW); digitalWrite(IN2, LOW); 
+        analogWrite(EN2, pwm) ; digitalWrite(IN3, LOW ); digitalWrite(IN4, LOW); 
     }     
     else {
         if(pwm < -30){
@@ -28,12 +27,9 @@ void Control(int pwm){
 }
 
 void PID(float pid_angle, float dt, float *output){
-    currentAngle = pid_angle;
-    error =  startPoint - currentAngle;
-
+    error =  startPoint - pid_angle;
     PID_integral  += error  * dt;
     PID_derivative = (error - preError) / dt;
-
     *output = Kp * error + Ki * PID_integral + Kd * PID_derivative;
     preError = error;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,11 @@
 #include "control_engine.h"
 
 extern int16_t ax, ay ,az, temp, gx, gy, gz;
-extern int PID_output;
 extern float startPoint;
 unsigned long lasttime;
-extern float previousAngle, currentAngle;
 bool once = true;
 float Pitch, Roll;
-float original_sumPitch = 0;
+float init_SumPitch = 0;
 double delta_time = 0 ;
 
 void setup(){
@@ -35,21 +33,20 @@ void setup(){
                 MPU_Getvalue();
                 Convert_Pitch_Raw();
                 MPU_Kalman_filter(&Pitch, &Roll);
-                original_sumPitch += Pitch;
+                init_SumPitch += Pitch;
             }
-            startPoint    = original_sumPitch / 20.0;
-            previousAngle = startPoint;
-            currentAngle  = startPoint;
+            startPoint    = init_SumPitch / 20.0;
+            //currentAngle  = startPoint;
         }
         offset();
     }
     
 }
-void T(){
-    static unsigned long timeT = 0;
-    Serial.println(micros() - timeT);
-    timeT = micros();
-}
+// void T(){
+//     static unsigned long timeT = 0;
+//     Serial.println(micros() - timeT);
+//     timeT = micros();
+// }
 float output = 0;
 void loop(){
     MPU_Getvalue();
